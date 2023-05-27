79b9b506872ad2853e9e998309ce0f93d9251b5a
p4nic <97530074+p4nickkj@users.noreply.github.com>
2023-05-28 00:18:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+
 import cv2
 import time
 
-fps = 60
+fps = 50
 tempo_gravacao = 5
 largura, altura = 640, 480
 
-captura = cv2.VideoCapture(0)
-captura.set(3, largura)
-captura.set(4, altura)
+def reiniciar_captura():
+    global captura
+    captura.release()
+    time.sleep(2)
+    captura = cv2.VideoCapture(0)
+    captura.set(3, largura)
+    captura.set(4, altura)
 
 class MotionDetector:
     def __init__(self, largura, altura, tempo_gravacao, fps):
@@ -15,9 +20,6 @@ class MotionDetector:
         self.altura = altura
         self.tempo_gravacao = tempo_gravacao
         self.fps = fps
-        self.quadro_anterior = None
-        self.tempo_inicio = None
-        self.gravando = False
         self.subtrator = cv2.createBackgroundSubtractorMOG2()
 
     def processar_quadro(self, quadro):
@@ -36,31 +38,40 @@ class MotionDetector:
                 cv2.rectangle(quadro, (x, y), (x + w, y + h), (255, 0, 255), 2)
                 movimento_detectado = True
 
-        if movimento_detectado:
-            if not self.gravando:
-                self.tempo_inicio = time.time()
-                self.gravando = True
-
-        elif self.gravando:
-            if time.time() - self.tempo_inicio >= self.tempo_gravacao:
-                self.gravando = False
-
-    def reset(self):
-        self.quadro_anterior = None
-        self.tempo_inicio = None
-        self.gravando = False
+        return movimento_detectado
 
 detector = MotionDetector(largura, altura, tempo_gravacao, fps)
+gravando = False
+tempo_inicio = None
+video_writer = None
+
+captura = cv2.VideoCapture(0)
+captura.set(3, largura)
+captura.set(4, altura)
 
 while True:
     try:
         ret, quadro = captura.read()
 
-        detector.processar_quadro(quadro)
+        if not ret:
+            print("Erro na leitura do quadro. Reiniciando...")
+            reiniciar_captura()
+            continue
 
-        if detector.gravando:
-            if 'video_writer' not in locals():
-                nome_arquivo = f"video_{int(time.time())}.mp4"
+        movimento_detectado = detector.processar_quadro(quadro)
+
+        if movimento_detectado:
+            if not gravando:
+                tempo_inicio = time.time()
+                gravando = True
+
+        elif gravando:
+            if time.time() - tempo_inicio >= tempo_gravacao:
+                gravando = False
+
+        if gravando:
+            if video_writer is None:
+                nome_arquivo = f"gravacao_{int(time.time())}.mp4"
                 video_writer = cv2.VideoWriter(nome_arquivo, cv2.VideoWriter_fourcc(*'mp4v'), fps, (largura, altura))
             video_writer.write(quadro)
 
@@ -73,16 +84,15 @@ while True:
     except cv2.error as exception:
         print("Ocorreu um erro:", exception)
         print("A reiniciar o programa...")
-        captura.release()
-        cv2.destroyAllWindows()
-        time.sleep(2)
-        captura = cv2.VideoCapture(0)
-        captura.set(3, largura)
-        captura.set(4, altura)
-        detector.reset()
+        reiniciar_captura()
+        gravando = False
+        tempo_inicio = None
+        if video_writer is not None:
+            video_writer.release()
+        video_writer = None
         continue
 
 captura.release()
-if 'video_writer' in locals():
+if video_writer is not None:
     video_writer.release()
 cv2.destroyAllWindows()
